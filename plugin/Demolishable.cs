def127cdbe5e912a58b2137a233b65e9cd8cb102
Romano Kleinwächter <romano.kleinwaechter@gmail.com>
2022-02-03 13:56:27 +0100

    Added demolishable plugin

---
+++ plugin/Demolishable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using ConVar;
+
+namespace Oxide.Plugins
+{
+    [Info("Demolishable", "Romano (RKL.tv) Kleinwaechter", "1.0.0")]
+    [Description("Disables demolishable timer for building blocks")]
+    public class Demolishable : CovalencePlugin
+    {
+        private void Init()
+        {
+            Puts("Demolishable loaded");
+        }
+
+        private void OnEntityBuilt(Planner plan, GameObject objectBlock)
+        {
+            var block = (objectBlock?.ToBaseEntity() ?? null) as BuildingBlock;
+            if (block)
+            {
+                this.disableStopTimerOf(block);
+            }
+        }
+
+        private void OnStructureUpgrade(BuildingBlock block, BasePlayer player, BuildingGrade.Enum grade)
+        {
+            this.disableStopTimerOf(block);
+        }
+        
+        private object OnHammerHit(BasePlayer player, HitInfo hitInfo)
+        {
+            var block = (hitInfo?.HitEntity ?? null) as BuildingBlock;
+            if (block)
+            {
+                this.disableStopTimerOf(block);
+            }
+            
+            return null;
+        }
+
+        private void disableStopTimerOf(BuildingBlock block)
+        {
+            block.CancelInvoke(block.StopBeingDemolishable); // disable old timer
+            block.SetFlag(BaseEntity.Flags.Reserved2, true); // enables demolishable
+        }
+    }
+}
